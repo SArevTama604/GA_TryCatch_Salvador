3b201c759193be1b7ce86b4d9a9539e3f3ab4ebe
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-03 17:04:17 -0800

    upload program

--- Program.cs
+++ Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        public static object Ex { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = 0;
+
+            try
+            {
+                Console.WriteLine("Please enter a whole number: ");
+                String userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+            }
+            catch(FormatException fex) {
+
+                Console.WriteLine("This was a format exception");
+                number = 12;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                number = 42;
+            }
+
+            Console.WriteLine(number)
+
+
+            ;
         }
     }
 }
